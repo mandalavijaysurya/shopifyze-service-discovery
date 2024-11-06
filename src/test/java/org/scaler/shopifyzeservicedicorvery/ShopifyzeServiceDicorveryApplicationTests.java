64cce5f7db87f09e0c357501752fbfbdf2a5fcd2
Lynx <mandala.vijay.surya@gmail.com>
2024-11-06 18:44:31 +0530

    feat: add service discovery

---
+++ src/test/java/org/scaler/shopifyzeservicedicorvery/ShopifyzeServiceDicorveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.shopifyzeservicedicorvery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyzeServiceDicorveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
